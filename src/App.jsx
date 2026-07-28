e192010bbbcf02bee473c7e38fe92998792be489
Dias Naufal <diasnaufal9@gmail.com>
2026-07-28 16:14:43 +0700

    ganti nama

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// Layout
+
 import AdminLayout from './components/layout/AdminLayout';
 
 // Routes
