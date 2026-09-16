a18d02e5f5b6c7c32db2a06378e681df4e7218e3
Dias Naufal <diasnaufal9@gmail.com>
2026-09-16 22:53:44 +0700

    Course

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://be-video-belajar-production.up.railway.app/api/v1/courses';
+const API_URL = 'https://be-video-belajar-production.up.railway.app/api/v1/course';
 export const getCourses = async () => {
     const response = await axios.get(API_URL);
     return response.data.data || [];
